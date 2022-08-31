e5d3ba6e2ddf98a0357b06dc6c92a87267ca4f67
habeeb-v <emekomahabeeb10@gmail.com>
2022-08-31 17:23:19 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let checkBox  = document.getElementById('toggle');
+let body = document.body;
+let dark = document.getElementById('dark');
+let light = document.getElementById('light');z
+
+checkBox.addEventListener('click', function(){
+    if(checkBox.checked){
+        body.classList.add('dark');
+        dark.style.display = 'none';
+        light.style.display = 'inline-block';
+    } else{
+        body.classList.remove('dark');
+        dark.style.display = 'inline-block';
+        light.style.display = 'none';
+    };
+});
